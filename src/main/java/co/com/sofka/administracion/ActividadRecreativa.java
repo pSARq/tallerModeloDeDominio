5219c067ddf9491bb518f169aafa37125d591e1c
Santiago Almendra <santiagoalmendrarojas19@gmail.com>
2021-12-20 17:30:43 -0500

    Se creó la entidad ActividadRecreativa

--- src/main/java/co/com/sofka/administracion/ActividadRecreativa.java
+++ src/main/java/co/com/sofka/administracion/ActividadRecreativa.java
@@ -1,4 +1,49 @@
 package co.com.sofka.administracion;
 
-public class ActividadRecreativa {
+import co.com.sofka.administracion.values.Actividad;
+import co.com.sofka.administracion.values.IdActividadRecreativa;
+import co.com.sofka.administracion.values.Material;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class ActividadRecreativa extends Entity<IdActividadRecreativa> {
+
+    private String nombre;
+    private Material material;
+    private Actividad actividad;
+
+    public ActividadRecreativa(IdActividadRecreativa entityId, String nombre, Material material, Actividad actividad) {
+        super(entityId);
+        this.nombre = nombre;
+        this.material = material;
+        this.actividad = actividad;
+    }
+
+    public void modificarNombre(String nombre){
+        if (nombre.isBlank()){
+            new IllegalArgumentException("El nombre no puede estar vacio");
+        }
+        this.nombre = nombre;
+    }
+
+    public void modificarMaterial(Material material){
+        this.material = Objects.requireNonNull(material);
+    }
+
+    public void modificarActividad(Actividad actividad){
+        this.actividad = Objects.requireNonNull(actividad);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public Actividad getActividad() {
+        return actividad;
+    }
 }
--- src/main/java/co/com/sofka/administracion/Guia.java
+++ src/main/java/co/com/sofka/administracion/Guia.java
@@ -19,6 +19,9 @@ public class Guia extends Entity<IdGuia> {
     }
 
     public void modificarCapacidadClientes(int capacidadClientes){
+        if (String.valueOf(capacidadClientes) == null){
+            new IllegalArgumentException("La capacidad de clientes no puede ser nula");
+        }
         if (capacidadClientes <= 0){
             new IllegalArgumentException("La capacidad de clientes del guia debe ser mayor de 0");
         }
